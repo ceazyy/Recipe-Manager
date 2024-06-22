05c747877442cc6c5a9cfe9c62ba1f87176896b9
Nipun Chandra <122171468+ceazyy@users.noreply.github.com>
2024-06-23 00:50:25 +0530

    Update addrecipewindow.h

--- addrecipewindow.h
+++ addrecipewindow.h
@@ -2,13 +2,15 @@
 #define ADDRECIPEWINDOW_H
 
 #include <QDialog>
-#include "database.h"
+#include "database.h"  // Include your Database class header
+#include "recipe.h"    // Include your Recipe class header
 
 namespace Ui {
 class AddRecipeWindow;
 }
 
-class AddRecipeWindow : public QDialog {
+class AddRecipeWindow : public QDialog
+{
     Q_OBJECT
 
 public:
@@ -17,6 +19,7 @@ public:
 
 private slots:
     void on_addButton_clicked();
+    void on_backButton_clicked();  // New slot for back button
 
 private:
     Ui::AddRecipeWindow *ui;
