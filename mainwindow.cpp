187749d06a39d8aedbb3f2538d1cd39f464c909f
Nipun Chandra <122171468+ceazyy@users.noreply.github.com>
2024-06-22 23:32:11 +0530

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,1 +1,31 @@
+// mainwindow.cpp
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    addRecipeDialog = new AddRecipeDialog(this);
+
+    connect(ui->addRecipeButton, &QPushButton::clicked, this, &MainWindow::openAddRecipeDialog);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::openAddRecipeDialog()
+{
+    addRecipeDialog->clearFields(); // Optional: Clear fields if dialog is reused
+    addRecipeDialog->show();
+}
+
+void MainWindow::addRecipe(QString name, QString portionSize, QList<QPair<QString, QString>> ingredients, QString notes)
+{
+    // Handle adding recipe data here (e.g., add to database or store in memory)
+    // Update UI to reflect new recipe added
+}
 
