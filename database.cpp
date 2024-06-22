a98bc8b8122bd01fad077015f9e5887b6c551f1e
Nipun Chandra <122171468+ceazyy@users.noreply.github.com>
2024-06-23 00:30:57 +0530

    Update database.cpp

--- database.cpp
+++ database.cpp
@@ -1,79 +1,78 @@
-// database.cpp
 #include "database.h"
 #include <QSqlQuery>
 #include <QSqlError>
 #include <QDebug>
 
-QSqlDatabase Database::db = QSqlDatabase::addDatabase("QSQLITE");
-
-bool Database::initialize()
-{
-    db.setDatabaseName("recipes.db");
-
-    if (!db.open()) {
-        qDebug() << "Error: Failed to open database:" << db.lastError().text();
-        return false;
-    }
-
-    QSqlQuery query;
-    query.exec("CREATE TABLE IF NOT EXISTS recipes ("
-               "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-               "name TEXT NOT NULL,"
-               "portion_size TEXT,"
-               "ingredients TEXT,"
-               "notes TEXT"
-               ")");
-
-    if (query.lastError().isValid()) {
-        qDebug() << "Error: Failed to create table:" << query.lastError().text();
-        return false;
-    }
-
-    return true;
+Database::Database() {
+    m_db = QSqlDatabase::addDatabase("QSQLITE");
+    m_db.setDatabaseName("recipes.db");
 }
 
-bool Database::addRecipe(const QString &name, const QString &portionSize, const QList<QPair<QString, QString>> &ingredients, const QString &notes)
-{
-    QSqlQuery query;
-    query.prepare("INSERT INTO recipes (name, portion_size, ingredients, notes) VALUES (:name, :portionSize, :ingredients, :notes)");
-    query.bindValue(":name", name);
-    query.bindValue(":portionSize", portionSize);
-
-    QStringList ingredientList;
-    for (const auto &ingredient : ingredients) {
-        ingredientList.append(ingredient.first + ": " + ingredient.second);
-    }
-    query.bindValue(":ingredients", ingredientList.join("; "));
-
-    query.bindValue(":notes", notes);
-
-    if (!query.exec()) {
-        qDebug() << "Error: Failed to add recipe:" << query.lastError().text();
-        return false;
-    }
-
-    return true;
+bool Database::open() {
+    return m_db.open();
 }
 
-QList<Recipe> Database::filterRecipes(const QString &type)
-{
+void Database::close() {
+    m_db.close();
+}
+
+bool Database::createRecipeTable() {
+    QSqlQuery query;
+    query.prepare("CREATE TABLE IF NOT EXISTS recipes ("
+                  "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                  "name TEXT NOT NULL,"
+                  "type TEXT,"
+                  "cuisine TEXT,"
+                  "ingredients TEXT,"
+                  "instructions TEXT,"
+                  "tags TEXT"
+                  ")");
+    return query.exec();
+}
+
+bool Database::addRecipe(const Recipe& recipe) {
+    QSqlQuery query;
+    query.prepare("INSERT INTO recipes (name, type, cuisine, ingredients, instructions, tags) "
+                  "VALUES (:name, :type, :cuisine, :ingredients, :instructions, :tags)");
+    query.bindValue(":name", recipe.getName());
+    query.bindValue(":type", recipe.getType());
+    query.bindValue(":cuisine", recipe.getCuisine());
+    query.bindValue(":ingredients", recipe.getIngredients());
+    query.bindValue(":instructions", recipe.getInstructions());
+    query.bindValue(":tags", recipe.getTags().join(", "));
+    return query.exec();
+}
+
+QList<Recipe> Database::getRecipes(const QString& type, const QString& cuisine, const QList<QString>& tags) {
     QList<Recipe> recipes;
-
     QSqlQuery query;
-    query.prepare("SELECT * FROM recipes WHERE type = :type");
-    query.bindValue(":type", type);
+    QString queryString = "SELECT * FROM recipes WHERE 1=1";
 
+    if (!type.isEmpty())
+        queryString += " AND type = '" + type + "'";
+    if (!cuisine.isEmpty())
+        queryString += " AND cuisine = '" + cuisine + "'";
+    if (!tags.isEmpty()) {
+        for (const QString& tag : tags) {
+            queryString += " AND tags LIKE '%" + tag + "%'";
+        }
+    }
+
+    query.prepare(queryString);
     if (query.exec()) {
         while (query.next()) {
             Recipe recipe;
-            recipe.setId(query.value("id").toInt());
             recipe.setName(query.value("name").toString());
-            recipe.setPortionSize(query.value("portion_size").toString());
-            // Parse ingredients and notes as needed
+            recipe.setType(query.value("type").toString());
+            recipe.setCuisine(query.value("cuisine").toString());
+            recipe.setIngredients(query.value("ingredients").toString());
+            recipe.setInstructions(query.value("instructions").toString());
+            QStringList tagList = query.value("tags").toString().split(", ");
+            recipe.setTags(tagList);  // Assuming setTags is appropriately implemented in Recipe
             recipes.append(recipe);
         }
     } else {
-        qDebug() << "Error: Failed to fetch recipes:" << query.lastError().text();
+        qDebug() << "Error querying recipes:" << query.lastError().text();
     }
 
     return recipes;
