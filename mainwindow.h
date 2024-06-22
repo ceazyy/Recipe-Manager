a4616c836bfaf9469e00b44f36adbea8f035d302
Nipun Chandra <122171468+ceazyy@users.noreply.github.com>
2024-06-22 23:30:49 +0530

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,1 +1,26 @@
+// mainwindow.h
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
 
+#include <QMainWindow>
+#include "addrecipedialog.h"
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void openAddRecipeDialog();
+    void addRecipe(QString name, QString portionSize, QList<QPair<QString, QString>> ingredients, QString notes);
+
+private:
+    Ui::MainWindow *ui;
+    AddRecipeDialog *addRecipeDialog;
+    // Add data structures or SQLite database connection here
+};
+
+#endif // MAINWINDOW_H
