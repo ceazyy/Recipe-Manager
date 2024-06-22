3da52272ae6b4b68ca15bfb70f6dcdda65a1ee23
Nipun Chandra <122171468+ceazyy@users.noreply.github.com>
2024-06-22 23:30:33 +0530

    Update database.h

--- database.h
+++ database.h
@@ -1,1 +1,26 @@
+#ifndef DATABASE_H
+#define DATABASE_H
 
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+#include <QtSql/QSqlError>
+#include <QDebug>
+#include <QList> // Include QList for using QList<QPair<...>>
+
+// Include QPair for using QPair<QString, QString>
+#include <QPair>
+#include <QString>
+#include <recipe.h>
+
+class Database
+{
+public:
+    static bool initialize();
+    static bool addRecipe(const QString &name, const QString &portionSize, const QList<QPair<QString, QString>> &ingredients, const QString &notes);
+    static QList<Recipe> filterRecipes(const QString &type);
+
+private:
+    static QSqlDatabase db;
+};
+
+#endif // DATABASE_H
